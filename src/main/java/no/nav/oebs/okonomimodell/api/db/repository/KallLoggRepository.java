a4552ae0b4306584e43518cf81760e3a179f4fc9
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-03-27 10:15:31 +0100

    chore(OEBS-1927): change name to okonomimodell-api

---
+++ src/main/java/no/nav/oebs/okonomimodell/api/db/repository/KallLoggRepository.java
@@ -0,0 +1,15 @@
+package no.nav.oebs.okonomimodell.api.db.repository;
+
+import no.nav.oebs.okonomimodell.api.db.entity.KallLogg;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Profile("!local & !t1")
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public interface KallLoggRepository extends JpaRepository<KallLogg, Integer>, KallLoggRepositoryCustom {
+
+}
